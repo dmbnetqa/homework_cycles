a5eb2572470342194974f7a19fd929620fd12d25
Дамба Манджиев <mdamba2020@yandex.ru>
2024-03-06 08:34:16 +0300

    add homework_cycles exercise_2

--- src/test/java/calcServiceTest.java
+++ src/test/java/calcServiceTest.java
@@ -1,19 +1,21 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.homework_cycles.services.calcService;
 
 public class calcServiceTest {
 
-@Test
-    public void monthsVacation() {
+@ParameterizedTest
+@CsvFileSource(files = "src/test/resources/data.csv")
+    public void monthsVacation(int expected, int income, int expenses,int threshold) {
 
         calcService service = new calcService();
 
-        int expected = 3;
+    //    int expected = 3;
    // int income = 10_000;
    // int expenses= 3_000;
   //  int threshold = 20_000;
-    int actual = service.Calculate(10_000, 3_000, 20_000);
+    int actual = service.Calculate(income, expenses, threshold);
 
     Assertions.assertEquals(expected, actual);
 
